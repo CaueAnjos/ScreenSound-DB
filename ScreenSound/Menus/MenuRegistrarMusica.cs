6cfb0fc73fdb4a6382b742c1a0f82f9012d97749
CaueAnjos <kawidosanjos@outlook.com>
2025-07-16 22:34:07 -0300

    feat(menu): add a way to register musics with authors

--- ScreenSound/Menus/MenuRegistrarMusica.cs
+++ ScreenSound/Menus/MenuRegistrarMusica.cs
@@ -11,6 +11,13 @@ internal class MenuRegistrarMusica : Menu
 
         Console.Write("Agora digite o título da música: ");
         string tituloDaMusica = Console.ReadLine()!;
+
+        Console.Write("Agora digite o nome do autor desta música: ");
+        string artistaNome = Console.ReadLine()!;
+
+        Musica musica = new Musica(tituloDaMusica);
+        musica.Artista = dal.Artistas.Get(a => a.Nome == artistaNome);
+
         dal.Musicas.Add(new Musica(tituloDaMusica));
         Console.WriteLine($"A música {tituloDaMusica} foi registrada com sucesso!");
         Thread.Sleep(4000);
