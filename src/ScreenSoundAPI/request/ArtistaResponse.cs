460519a537e98430e361839d728f6690c64164b6
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2025-09-20 18:56:00 -0300

    feat(request): add musics to ArtistaResponse

--- src/ScreenSoundAPI/request/ArtistaResponse.cs
+++ src/ScreenSoundAPI/request/ArtistaResponse.cs
@@ -2,12 +2,13 @@ using ScreenSoundCore.Modelos;
 
 namespace ScreenSoundAPI.Request;
 
-public record ArtistaResponse(int Id, string Name, string Bio, string FotoPerfil);
+public record ArtistaResponse(int Id, string Name, string Bio, string FotoPerfil, ICollection<MusicaResponse> Musics);
 
 public static class ArtistaResponseExtations
 {
     public static ArtistaResponse GetResponse(this Artista artista)
     {
-        return new ArtistaResponse(artista.Id, artista.Nome, artista.Bio, artista.FotoPerfil);
+        var musics = artista.Musicas.Select(m => m.GetResponse()).ToArray();
+        return new ArtistaResponse(artista.Id, artista.Nome, artista.Bio, artista.FotoPerfil, musics);
     }
 }
