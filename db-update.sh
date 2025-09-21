cc563861054046a623ef3c486fcf3b37d77d26e0
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2025-09-21 13:15:04 -0300

    feat(docker): update docker outomation script

--- db-update.sh
+++ db-update.sh
@@ -1,8 +1,19 @@
-#!/bim/bash
+#!/bin/bash
+
+project=./src/ScreenSoundCore/ScreenSoundCore.csproj
+
+echo -n "Do you want to reset db for update?[y/n]: "
+read -r reset
+
+if [[ $reset == 'y' ]] || [[ $reset == 'Y' ]]; then
+    echo "Reseting db"
+    docker compose down
+    docker compose up
+fi
 
 echo "Starting to update the database..."
 
 docker compose start
-dotnet ef database update --project ScreenSoundCore
+dotnet ef database update --project $project
 
 echo "Done! Your database is now up to date."
