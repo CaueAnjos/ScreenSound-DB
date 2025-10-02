e424d5ad2ca7cf6680c0a69e25b3fff91b3439c9
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2025-10-01 22:16:11 -0300

    feat(request): add UpdateMusicaRequest

--- src/ScreenSoundAPI/request/MusicaRequest.cs
+++ src/ScreenSoundAPI/request/MusicaRequest.cs
@@ -5,8 +5,27 @@ namespace ScreenSoundAPI.Request;
 
 public record MusicaRequest(string Name, ICollection<GeneroRequest> Generos);
 
+// NOTE: this is use on UpdateMusica!
+public record UpdateMusicaRequest(int Id, string Name, DateTime Date, ICollection<GeneroRequest> Generos, int ArtistaId = -1);
+
 public static class MusicaRequestExtations
 {
+    public static bool TryUpdateObject(this UpdateMusicaRequest request, IDal db)
+    {
+        int id = request.Id;
+        var musicToUpdate = db.Musicas.GetById(id);
+        if (musicToUpdate is null)
+            return false;
+
+        var artista = db.Artistas.GetById(request.ArtistaId);
+        musicToUpdate.Artista = artista is not null ? artista : musicToUpdate.Artista;
+
+        musicToUpdate.DataLancamento = request.Date != DateTime.MinValue ? request.Date : musicToUpdate.DataLancamento;
+
+        db.Musicas.Update(musicToUpdate);
+        return true;
+    }
+
     public static Musica? TryGetObject(this MusicaRequest musica, IDal db)
     {
         return db.Musicas.GetSingle(a => a.Nome == musica.Name);
