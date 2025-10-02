08fdd450bc661bd23edf54cd5441b9bf2921f6eb
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2025-10-01 22:25:40 -0300

    feat(response): add music id to response

--- src/ScreenSoundAPI/request/MusicaResponse.cs
+++ src/ScreenSoundAPI/request/MusicaResponse.cs
@@ -2,7 +2,7 @@ using ScreenSoundCore.Modelos;
 
 namespace ScreenSoundAPI.Request;
 
-public record MusicaResponse(string Name, DateTime DataLancamento, int? ArtistaId, ICollection<GeneroResponse> Generos);
+public record MusicaResponse(int Id, string Name, DateTime DataLancamento, int? ArtistaId, ICollection<GeneroResponse> Generos);
 
 public static class MusicaResponseExtations
 {
@@ -10,6 +10,6 @@ public static class MusicaResponseExtations
     {
         int? artistaId = musica.Artista?.Id ?? null;
         var generos = musica.Generos.Select(g => g.GetResponse()).ToList();
-        return new MusicaResponse(musica.Nome, musica.DataLancamento, artistaId, generos);
+        return new MusicaResponse(musica.Id, musica.Nome, musica.DataLancamento, artistaId, generos);
     }
 }
