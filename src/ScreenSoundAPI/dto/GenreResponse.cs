3ecfc11c29b473ae786a6a804e498181cda4f8ba
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2025-10-29 20:35:21 -0300

    feat: add DefaultGenreResponse convetion method

--- src/ScreenSoundAPI/dto/GenreResponse.cs
+++ src/ScreenSoundAPI/dto/GenreResponse.cs
@@ -2,7 +2,13 @@ using ScreenSoundCore.Modelos;
 
 namespace ScreenSoundAPI.dto;
 
-public record DefaultGenreResponse(string Name, string Description);
+public record DefaultGenreResponse(string Name, string Description)
+{
+    public static implicit operator DefaultGenreResponse(Genre genre)
+    {
+        return new DefaultGenreResponse(genre.Name, genre.Description);
+    }
+}
 
 public static class GeneroResponseExtations
 {
