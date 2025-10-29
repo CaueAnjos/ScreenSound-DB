4a3e173a2d3b2c1a31b158a534b8be5c5a7a2b45
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2025-10-28 22:08:18 -0300

    fix: fix naming in portuguese

---
+++ src/ScreenSoundCore/Migrations/20251029012017_FixGenreDescription.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSoundCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixGenreDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Descricao",
+                table: "Genres",
+                newName: "Description");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Genres",
+                newName: "Descricao");
+        }
+    }
+}
