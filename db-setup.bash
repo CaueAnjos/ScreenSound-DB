caf4cd1d1cb355b41195542c898b60776e27b91e
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2025-11-20 19:45:10 -0300

    fix: fix db-setup

--- db-setup.bash
+++ db-setup.bash
@@ -3,14 +3,21 @@
 repo=https://github.com/CaueAnjos/ScreenSound-DB.git
 project=./src/ScreenSoundCore/ScreenSoundCore.csproj
 
-echo -n "Do you want to setup db for ScreenSound?[y/n]: "
+echo -n "Do you want to setup db for ScreenSound? [y/n]: "
 read -r setup
 
 if [[ $setup == 'y' ]] || [[ $setup == 'Y' ]]; then
-    echo "cloning, momentaraly, the repo CaueAnjos/ScreenSound-DB"
+    echo "Cloning repository..."
     git clone $repo --depth 1
-    echo "setting up db"
-    docker compose ScreenSound-DB/compose.yaml up -d
+    cd ScreenSound-DB || exit
+
+    echo "Starting Docker Compose services..."
+    docker compose up -d
+
+    echo "Running database migrations..."
     dotnet ef database update --project $project
+
     echo "Done! Your database is now up to date."
+
+    cd ..
 fi
