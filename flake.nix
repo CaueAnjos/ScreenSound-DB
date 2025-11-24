0224e1e898be89741eaf7d554b33c8f36b2a45c7
CaueAnjos <141049846+CaueAnjos@users.noreply.github.com>
2025-11-24 15:25:12 -0300

    feat: add app.default to esealy run the api

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = {
+    self,
     flake-utils,
     nixpkgs,
     ...
@@ -50,5 +51,31 @@
             "${placeholder "out"}/lib/${pname}"
           ];
         };
+
+      apps.default = {
+        type = "app";
+        program = "${pkgs.writeShellScriptBin "run" ''
+          set -e
+
+          cleanup() {
+            echo "Stopping database container..."
+            docker stop ScreenSound-DB 2>/dev/null || true
+          }
+          trap cleanup EXIT
+
+          echo "Starting SQL Server container..."
+          docker run --rm -d \
+            --name ScreenSound-DB \
+            -p 1433:1433 \
+            -e ACCEPT_EULA=Y \
+            -e MSSQL_SA_PASSWORD='[Senha123]' \
+            mcr.microsoft.com/mssql/server:2022-latest
+
+          sleep 10
+          echo "SQL Server is ready!"
+          echo "Starting API..."
+          exec ${self.packages.${system}.default}/bin/ScreenSoundAPI
+        ''}/bin/run";
+      };
     });
 }
